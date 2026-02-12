b10c95bf83bdfbfe66c1e4dcaf31503712963282
Malick Doukoure <bh390824@ens.uqam.ca>
2026-02-12 15:21:45 -0500

    Config Render + build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
-#!/usr/bin/env bash
+echo '#!/usr/bin/env bash
 set -o errexit
-
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py migrate' > build.sh
+chmod a+x build.sh
